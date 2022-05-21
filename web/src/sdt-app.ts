1326373f6770c7036760c8644ce21a182a6e5c34
Sandro Lain <xande05@gmail.com>
2022-05-21 22:11:25 +0200

    Setup of WASM based webapp

--- web/src/sdt-app.ts
+++ web/src/sdt-app.ts
@@ -1,7 +1,8 @@
-import { html, css, LitElement, PropertyValueMap } from 'lit'
-import { customElement, property, query, state } from 'lit/decorators.js'
+import { html, css, LitElement } from 'lit'
+import { customElement, query, state } from 'lit/decorators.js'
 import "./wasm_exec.js"
 import wasm from "./sdt.wasm?url";
+import "./main.css";
 
 interface Preset {
   name: string;
@@ -35,59 +36,137 @@ const presets: Preset[] = [{
 export class SdtApp extends LitElement {
   static styles = css`
     :host {
-      display: block;
-      border: solid 1px gray;
+      box-sizing: border-box;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
       padding: 16px;
-      max-width: 800px;
+      font-family: 'Courier New', Courier, monospace !important;
     }
+
+    h1 {
+      font-size: 1em;
+      margin: 0;
+      text-align: center;
+      line-height: 34px;
+      padding-top: 16px;
+    }
+
+    #top {
+      display: flex;
+      gap: 16px;
+    }
+    #top > div {
+      display: flex;
+      flex-direction: column;
+    }
+    #top > div:nth-of-type(2) {
+      flex: 1;
+    }
+
+    select, input, textarea, button {
+      font-family: 'Courier New', Courier, monospace !important;
+      background: var(--bg-color);
+      color: var(--tx-color);
+      border: 1px solid var(--tx-color);
+      padding: 8px;
+      font-size: 16px;
+      height: 34px;
+      line-height: 1em;
+      box-sizing: border-box;
+    }
+    button {
+      background: var(--tx-color);
+      color: var(--bg-color);
+      cursor: pointer;
+    }
+
+    textarea {
+      width: 100%;
+      height: 100%;
+      resize: none;
+    }
+
+    #bottom {
+      width: 100%;
+      display: flex;
+      gap: 16px;
+      flex: 1;
+    }
+    #bottom > div {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+    }
+
   `;
 
   @query("#input")
-  private $input: HTMLTextAreaElement;
+  private $input!: HTMLTextAreaElement;
 
   @query("#preset")
-  private $preset: HTMLSelectElement;
+  private $preset!: HTMLSelectElement;
 
   @query("#command")
-  private $command: HTMLInputElement;
+  private $command!: HTMLInputElement;
 
   @query("#output")
-  private $output: HTMLTextAreaElement;
+  private $output!: HTMLTextAreaElement;
 
   @state()
   private hideInput: boolean = false;
 
   render() {
     return html`
-      ${this.hideInput ? null : html`
-      <div id="input-wrp">
-        <textarea id="input"></textarea>
+      <div id="top">
+        <h1>Smart Developer Tools ^(;,;)^</h1>
+        <div>
+          <label for="preset">Preset</label>
+          <select id="preset" @change=${this.onPresetChange}>${presets.map((p) => html`<option value=${p.command}>${p.name}</option>`)}</select>
+        </div>
+        <div>
+          <label for="command">Command</label>
+          <input type="text" id="command" autocomplete="off" autocapitalize="off" spellcheck="false" />
+        </div>
+        <div>
+          <label for="execute">&nbsp;</label>
+          <button type="button" id="execute" @click=${this.onExecute}>Execute</button>
+        </div>
       </div>
-      `}
-      <div id="input-wrp">
-        <select id="preset" @change=${this.onPresetChange}>${presets.map((p) => html`<option value=${p.command}>${p.name}</option>`)}</select>
-        <input type="text" id="command" />
-        <button type="button" id="execute" @click=${this.onExecute}>Execute</button>
-      </div>
-      <div id="input-wrp">
-        <textarea id="output" readonly></textarea>
+      <div id="bottom">
+        ${this.hideInput ? null : html`
+        <div id="input-wrp">
+          <label for="input">Input</label>
+          <textarea id="input" autocomplete="off" autocapitalize="off" spellcheck="false"></textarea>
+        </div>
+        `}
+        <div id="output-wrp">
+          <label for="output">Output</label>
+          <textarea id="output" readonly></textarea>
+        </div>
       </div>
     `;
   }
 
-  private Go: Go;
+  private Go!: Go;
   private outputBuf: string = '';
+  private wasm!: ArrayBuffer;
 
   protected async firstUpdated(): Promise<void> {
+    this.wasm = await (await fetch(wasm)).arrayBuffer();
     this.Go = new Go();
-
-
     const decoder = new TextDecoder("utf-8");
     (window as any).fs.writeSync = (fd, buf) => {
       this.outputBuf += decoder.decode(buf);
-      this.$output.value = this.outputBuf;
+      this.applyOutput();
       return buf.length;
     };
+  }
+
+  private applyOutput(): void {
+    this.$output.value = this.outputBuf;
   }
 
   private onPresetChange() {
@@ -95,11 +174,13 @@ export class SdtApp extends LitElement {
     this.$command.value = value;
     const preset = presets.find((p) => (p.command === value));
     this.hideInput = !preset?.input;
+    this.outputBuf = "";
+    this.applyOutput();
   }
 
   private async onExecute() {
     this.outputBuf = "";
-    const wa = await WebAssembly.instantiateStreaming(fetch(wasm), this.Go.importObject);
+    const wa = await WebAssembly.instantiate(this.wasm, this.Go.importObject);
     const input = this.$input?.value ?? "";
     const args = this.$command.value.split(/\s+/);
     if(input) {
